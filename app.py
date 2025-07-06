cd08284349730409eb6cfaef169fff2536705803
Faisal Naseer <127278630+xtfaisal07@users.noreply.github.com>
2025-07-06 13:46:45 +0530

    app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ st.write("Enter the details below to predict the house price (in 100,000s):")
 
 # Input fields
 med_inc = st.number_input("Median Income (in lacs)", min_value=0.0, max_value=20.0, value=5.0)
-house_age = st.slider("House Age (in years)", 1, 100, 25)
+house_age = st.slider("House Age (in years)", 1, 100, 20)
 ave_rooms = st.number_input("Average Number of Rooms", min_value=1.0, max_value=50.0, value=5.0)
-ave_occup = st.number_input("Average Occupancy", min_value=1.0, max_value=10.0, value=3.0)
+ave_occup = st.number_input("Average Occupancy", min_value=1.0, max_value=10.0, value=2.0)
 
 # Predict button
 if st.button("Predict"):
